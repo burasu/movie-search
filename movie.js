cc0a58cf51babe48e45b17224c9124e907858c0d
Blas <burasu@gmail.com>
2018-04-18 20:37:07 +0200

    d

--- movie.js
+++ movie.js
@@ -15,23 +15,35 @@ app.use("/", express.static("public"));
 app.set("view engine", "pug");
 
 // The Movie DB parameters
-const API_KEY = "API_KEY";
+const API_KEY = "53c4427164154264b13c56b11bd4d755";
 let url = "https://api.themoviedb.org/3";
 let url_images = "http://image.tmdb.org/t/p/original";
 
 app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/search", (req, res) => {
-  res.redirect(301, "/");
+  console.log(req.query.search);
+  if (req.query.search == undefined) {
+    res.send("No hay parametros");
+  } else {
+    // Get the movie to find and pass it to the URL
+    const params = req.query;
+    if (params == {}) res.send("no hay parametros");
+    else {
+      const paramQuery = req.query.search;
+      console.log(paramQuery);
+      if (paramQuery == "") res.send("no hay nada");
+      else {
+        res.send("hay algo");
+        //console.log(req.query.search);
+        //res.render("index");
+      }
+    }
+  }
 });
 
 app.post("/search", (req, res) => {
-  // Get the movie to find and pass it to the URL
   const paramQuery = req.body.movie;
-  url = `${url}/search/movie?api_key=${API_KEY}&query=${paramQuery}&language=es-ES`;
 
+  url = `${url}/search/movie?api_key=${API_KEY}&query=${paramQuery}&language=es-ES`;
   request(url, (error, response, body) => {
     bodyContent = JSON.parse(body);
     //res.send(bodyContent);
@@ -40,10 +52,12 @@ app.post("/search", (req, res) => {
 });
 
 app.get("/list", (req, res) => {
-  const movies = bodyContent.results;
+  console.log(req);
+  res.send("hola");
+  /*   const movies = bodyContent.results;
   const hola = "hola";
   //res.send(movies);
-  res.render("list", { movies });
+  res.render("list", { movies }); */
 });
 
 app.listen(3000, () => {
