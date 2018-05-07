75b76fe7c2e36ecd9cb8d08c678414c044908455
Blas Fernández Segura <burasu@gmail.com>
2018-05-07 12:11:03 +0200

    Config de la api

--- themoviedb.js
+++ themoviedb.js
@@ -1,6 +1,6 @@
 // Parámetros de theMovieDb.org
 const themoviedb = {
-  API_KEY: "YOUR_API_KEY",
+  API_KEY: "53c4427164154264b13c56b11bd4d755",
   url: "https://api.themoviedb.org/3",
   url_images_original: "http://image.tmdb.org/t/p/original",
   url_images_w500: "http://image.tmdb.org/t/p/w300"
